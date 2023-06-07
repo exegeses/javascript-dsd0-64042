ee6e41c7c5a087f80eab1cb3318a364e8052aec2
Marcos <exegeses@gmail.com>
2023-06-07 12:09:45 -0300

    introducción a las funciones en javascript

---
+++ clase4/js/cambiarColor.js
@@ -0,0 +1,16 @@
+//ubicamos elemento dentro del DOM
+const caja = document.getElementById('caja');
+
+
+function colorVerde()
+{
+    caja.style.backgroundColor = '#f60';
+    caja.innerText = 'verde';
+}
+
+function colorNaranja()
+{
+    caja.style.backgroundColor = '#0a2';
+    caja.innerText = 'naranja';
+}
+        
